b5d427f8a8f5f0691e9820a4a572dca5d0c1cf83
OmarHassin <boky555555@gmail.com>
2020-07-05 19:28:25 +0200

    File Structure

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 20 }} />
+    </View>
+  );
+}
