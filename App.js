fc598389ec2fed412bc7f2db66739a14b7fc478b
OmarHassin <boky555555@gmail.com>
2020-07-06 14:22:37 +0200

    assets file

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ height: 20 }} />
+      <Text>Omar</Text>
     </View>
   );
 }
